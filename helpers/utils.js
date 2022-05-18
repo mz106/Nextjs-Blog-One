9b4031e61a6b628270f3809717730374372d299a
Michael Z <michaelzwierzanski@gmail.com>
2022-05-18 15:51:51 +0100

    uri env changed

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,7 +1,7 @@
 require('isomorphic-fetch');
 
 export async function getAllPosts() {
-    const url = process.env.FIREBASE_URI;
+    const url = process.env.NEXT_PUBLIC_FIREBASE_URI;
     const response = await fetch(`${url}/posts.json`);
     const data = await response.json();
 
